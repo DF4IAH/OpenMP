6a6482ad191ea8bec637e2a7c8eec5031b49b7af
Ulrich Habel <espero7757@gmx.net>
2018-11-30 23:27:04 +0100

    Exercise 4-4: single construct with implicit wait
    Signed-off-by: Ulrich Habel <espero7757@gmx.net>

---
+++ Ex4_4_single.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "omp.h"
+
+const int NUM_SLICES 	= 16;
+const int NUM_THREADS	=  4;
+
+
+int main(void)
+{
+	long id;
+
+	omp_set_num_threads(NUM_THREADS);
+
+	#pragma omp parallel
+	{
+		id = omp_get_thread_num();
+
+		#pragma omp single
+		{
+			id += 100;
+			printf("single: id = %ld\r\n", id);
+		}
+
+		id = omp_get_thread_num();
+		printf("id = %ld\r\n", id);
+	}
+        
+	printf("done.\n");
+        return 0;
+}
