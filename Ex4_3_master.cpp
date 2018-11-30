f08e36f90e8914cb6132ae6be3f633f7f79136ca
Ulrich Habel <espero7757@gmx.net>
2018-11-30 23:19:07 +0100

    Exercise 4-3: master construct
    Signed-off-by: Ulrich Habel <espero7757@gmx.net>

---
+++ Ex4_3_master.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "omp.h"
+
+const int NUM_SLICES 	= 16;
+const int NUM_THREADS	=  4;
+
+
+int main(void)
+{
+	long id;
+
+	omp_set_num_threads(NUM_THREADS);
+
+	#pragma omp parallel
+	{
+		id = omp_get_thread_num();
+
+		#pragma omp master
+		{
+			id += 100;
+			printf("master: id = %ld\r\n", id);
+		}
+
+		#pragma omp barrier
+
+		id = omp_get_thread_num();
+		printf("id = %ld\r\n", id);
+	}
+        
+	printf("done.\n");
+        return 0;
+}
