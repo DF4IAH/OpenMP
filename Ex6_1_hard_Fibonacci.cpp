231adcd220f509f00e74de3ad9af3ff8b55dece9
Ulrich Habel <espero7757@gmx.net>
2018-12-01 21:55:19 +0100

    Exercise 5-1: pi-by-dartboard
    Signed-off-by: Ulrich Habel <espero7757@gmx.net>

---
+++ Ex6_1_hard_Fibonacci.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "omp.h"
+
+const int NUM_SLICES 	= 90;
+//const int NUM_THREADS	=  4;
+
+static long fib[NUM_SLICES] = { 0, 1 };
+
+
+int main(void)
+{
+	long c;
+
+	#pragma omp parallel private(c)
+	{
+		#pragma omp for ordered
+		for (long idx = 0; idx < NUM_SLICES; idx++)
+		{
+			#pragma omp ordered
+			{
+				fib[idx + 2] = c = fib[idx] + fib[idx + 1];
+				printf("%02d: %ld\r\n", idx, c);
+			}
+		}
+	}
+
+        return 0;
+}
